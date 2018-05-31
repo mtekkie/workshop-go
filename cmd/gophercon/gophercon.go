24ad978beff9d061ecfac60f6a2eac2a0696b4e4
Robert Forsström <robert@middleware.se>
2018-05-31 13:18:18 +0200

    Added SERVICE port stuff

--- cmd/gophercon/gophercon.go
+++ cmd/gophercon/gophercon.go
@@ -4,15 +4,22 @@ import (
 	"net/http"
 	"log"
 	"github.com/mtekkie/workshop-go/pkg/routing"
+	"os"
 )
 
 // go run ./cmd/gophercon/gophercon.go
 func main() {
 
+	log.Printf("Service is starting")
+	port := os.Getenv("SERVICE_PORT")
+
+	if len(port) == 0 {
+		log.Fatal("Servivce port was not set")
+	}
+
 	r := routing.BaseRouter()
 
-	log.Printf("Service is starting")
-	http.ListenAndServe(":8000", r)
+	http.ListenAndServe(":"+port, r)
 }
 
 
